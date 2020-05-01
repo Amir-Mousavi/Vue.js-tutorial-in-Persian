229d68c1a52d1049bf9c1ffa880c89c56cd7f3c5
Amir <amir.mousavie@gmail.com>
2020-05-01 18:20:41 +0200

    Composing with Components

---
+++ src/components/GroceryItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{ item.text }}</div>
+</template>
+
+<script>
+export default {
+  props: ["item"]
+};
+</script>
+
+
+<style scoped>
+div {
+  margin: 5px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  text-align: left;
+}
+</style>
