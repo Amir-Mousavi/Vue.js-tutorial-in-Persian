b74ecb6ef4383cd854cda2d24e7d2e614a01634f
Amir <amir.mousavie@gmail.com>
2020-05-02 16:55:36 +0200

    lesson 06 Watchers

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 
 <template>
   <div id="app">
-    {{ reversedMessage }}
-
-    <p />
-
-    <div>
-      {{ fullName }}
-    </div>
+    <p>
+      Question: <input v-model="question" />
+    </p>
+    <p>
+      Answer: {{ answer }}
+    </p>
+    <p>
+      <img v-if="image" :src="image">
+    </p>
   </div>
 </template>
 
 <script>
+  import { debounce } from 'lodash';
 export default {
   name: "App",
   data() {
     return {
-      message: 'Goodbye',
-      firstName: 'John',
-      lastName: 'Due',
+      question: '',
+      answer: 'Please ask me a question...',
+      image: null,
     }
   },
-  computed: {
-    reversedMessage() {
-      return this.message.split('').reverse().join('');
-    },
-    fullName() {
-      return `${this.firstName} ${this.lastName}`
+  created() {
+    this.getAnswerDebounced = debounce(this.getAnswer, 500);
+  },
+  watch: {
+    question(newValue, oldValue) {
+      console.log({ newValue, oldValue });
+
+      this.getAnswerDebounced();
+    }
+  },
+  methods: {
+    getAnswer() {
+      if(this.question.indexOf('?') === -1) {
+        this.answer = 'Put a question mark in your question';
+        return;
+      }
+
+      fetch('https://yesno.wtf/api')
+        .then(response => {
+          response.json().then(data => {
+            this.answer = data.answer;
+            this.image = data.image;
+          })
+        })
+        .catch(error => {
+          this.answer = 'Something went wrong '+error;
+        })
     }
   }
 };
